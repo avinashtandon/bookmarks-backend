ab4b43515680a1ab5d40e24460531b8b82fcc602
avinashtandon <avinashtandon07@gmail.com>
2024-10-24 22:02:14 +0530

    V01.24.10.2024-alpha

---
+++ src/test/java/com/bookmarks/backend/BookmarksBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookmarks.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmarksBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
